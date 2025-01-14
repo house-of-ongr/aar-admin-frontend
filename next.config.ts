d326788732881e0208ec5b833e5561564a5942c4
SUNYOUNG YOON <hh24010016@gmail.com>
2025-01-15 02:56:57 +0900

    feat : 이미지 로드시 필요한 next.config 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME_TEST,
+        port: process.env.NEXT_PUBLIC_IMAGE_PORT_TEST,
+        pathname: process.env.NEXT_PUBLIC_IMAGE_PATH_TEST,
+      },
+      {
+        protocol: "http",
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME_PRIVATE,
+        port: "",
+        pathname: process.env.NEXT_PUBLIC_IMAGE_PATH_PRIVATE,
+        search: "",
+      },
+      {
+        protocol: "http",
+        hostname: process.env.NEXT_PUBLIC_IMAGE_HOSTNAME_PUBLIC,
+        port: "",
+        pathname: process.env.NEXT_PUBLIC_IMAGE_PATH_PUBLIC,
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
