1e942bdc879d8cb2c471201baa2038d84945ccde
SUNYOUNG YOON <hh24010016@gmail.com>
2025-01-25 02:15:21 +0900

    remove : test page and test image files

--- src/app/test/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPageLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <section>
-      <main className="h-screen ">{children}</main>
-    </section>
-  );
-}
